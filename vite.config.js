fe9acba53c13980d8fcf9217e6c84d42a45c252c
Edinson Fernandez <142480192+AnotherEz@users.noreply.github.com>
2025-02-25 15:04:09 -0500

    1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Asegúrate de que 'dist' sea el directorio de salida
-  },
 })
+
