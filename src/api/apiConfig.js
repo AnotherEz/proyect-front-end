fb6242f08f6d19f996460ae623f8b62dfeefc676
Edinson Fernandez <142480192+AnotherEz@users.noreply.github.com>
2025-02-25 19:13:02 -0500

    1

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -5,7 +5,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true, // Esto es importante para sesiones y cookies
+  withCredentials: true, 
 });
 
 export default api;
